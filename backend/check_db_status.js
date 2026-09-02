d10c97786a71039911a022437c1f11a5c2d94a86
Arpan Yadav <arpan@proyotech.com>
2026-09-02 19:52:28 +0530

    chore: Remove leftover temp scripts

--- backend/check_db_status.js
+++
@@ -1,5 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.$connect()
-  .then(() => { console.log('DB_AWAKE'); process.exit(0); })
-  .catch((e) => { console.error('DB_ASLEEP', e.message); process.exit(1); });
